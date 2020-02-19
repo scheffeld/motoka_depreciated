fce74e5f7eba83602fa55ce4622985ceef4561e7
Roger Scheffeld <rscheffeld@gmail.com>
2020-02-19 18:32:12 -0300

    Criando o componente Item.

---
+++ src/components/Item/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ListItem, Left, Body, Right } from 'native-base';
+import { Icon } from 'react-native-elements';
+import { Text } from 'react-native';
+import styles from './styles'
+
+const ItemComponent = (props) => {
+    const item = props.item
+    return(
+        <ListItem key={item.id} style={styles.item} onPress={props.onPress}>
+            <Left>
+                <Text>{item.date}</Text>
+            </Left>
+            <Body>
+                <Text>{ item.type == 'oil' ? 'Troca de Óleo' : 'Abastecimento' }</Text>
+            </Body>
+            <Right>
+                <Icon name='keyboard-arrow-right' type='Font-Awesome5'/>
+            </Right>
+        </ListItem>
+    )
+};
+
+export default ItemComponent;
