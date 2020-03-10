d468328be18239a367b7f3330927efde38e11686
Roger Scheffeld <rscheffeld@gmail.com>
2020-03-10 12:31:32 -0300

    Alteração nos módulos e rotas do projeto.

--- App.js
+++ App.js
@@ -1,114 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator, HeaderStyleInterpolators } from 'react-navigation-stack';
+import { Icon } from 'react-native-elements'
+import { Root } from 'native-base'
+import { Alert } from 'react-native'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import LoginScreen from './src/screens/Login'
+import NewUserScreen from './src/screens/NewUser';
+import HomeScreen from './src/screens/Home';
+import NewRecordScreen from './src/screens/NewRecord';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const goBack = (navigation) => {
+  Alert.alert(
+    'Tem certeza?',
+    'Deseja voltar? Todas as informaçoes serão apagadas e as alteração não salvas serão desfeitas.',
+    [
+      { text: 'Sim', onPress: () => navigation.goBack()},
+      { text: 'Não', onPress: () => {}}
+    ]
+  )
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+const signout = (navigation) => {
+  Alert.alert(
+    'Tem certeza?',
+    'Deseja encerrar a sua sessão? Você será direcionado à tela de login.',
+    [
+      { text: 'Sim', onPress: () => navigation.navigate('Login')},
+      { text: 'Não', onPress: () => {}}
+    ]
+  )
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+const Navigation = createAppContainer(
+  createStackNavigator({
+    Login: {
+      screen: LoginScreen,
+      navigationOptions: {
+        header: () => false
+      }
+    },
+    'New User': {
+      screen: NewUserScreen,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: () => <Icon name='keyboard-arrow-left' type='Font-Awesome5' color='#FFF' onPress={() => goBack(navigation)}/>,
+        headerLeftContainerStyle: {
+          margin: 10
+        },
+        title: 'Novo Usuário'
+      })
+    },
+    'Home': {
+      screen: HomeScreen,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: () => <Icon name='exit-to-app' type='Font-Awesome5' color='#FFF' onPress={() => signout(navigation)}/>,
+        headerLeftContainerStyle: {
+          margin: 10
+        },
+        title: 'Motoka'
+      }),
+    },
+    'New Record': {
+      screen: NewRecordScreen,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: () => <Icon name='keyboard-arrow-left' type='Font-Awesome5' color='#FFF' onPress={() => goBack(navigation)}/>,
+        headerLeftContainerStyle: {
+          margin: 10
+        },
+        title: 'Novo Registro'
+      }),
+    }
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  {
+    initialRouteName: 'Login',
+    headerMode: 'screen',
+    mode: 'card',
+    defaultNavigationOptions:{
+      headerTitleAlign: 'center',
+      headerTintColor: '#FFF',
+      headerStyle: {
+          backgroundColor: '#7D40E7'
+      },
+      headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
+      headerPressColorAndroid: '#000',
+      cardShadowEnabled: false
+    }
+  })
+)
 
-export default App;
+export default () => <Root><Navigation/></Root>
